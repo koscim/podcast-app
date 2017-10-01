dc0d75a21d69dc5f146c8d9b542e01286b9a89f6
Monica Kosciuk <koscim@Monicas-MacBook-Pro.local>
2017-10-01 15:18:11 -0400

    Add Selenium and Capybara tests

--- spec/features/user_generates_episode_suggestions_spec.rb
+++ spec/features/user_generates_episode_suggestions_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
+def wait_until
+  Timeout.timeout(Capybara.default_max_wait_time) do
+    sleep(0.1) until value = yield
+    value
+  end
+end
+
+
+def wait_for_page_reload
+  id = find('html').native.ref
+  yield
+  wait_until { find('html').native.ref != id }
+end
+
 feature "user can generate episode suggestions", %() do
   scenario "user generates suggestions without subscriptions" do
     FactoryGirl.create(:user, first_name: 'Craig', last_name: 'Ternow', username: 'harrypotterfan1', email: 'harrypotterfan@hogwarts.com')
@@ -35,13 +49,32 @@ feature "user can generate episode suggestions", %() do
       description: 'The Protoss are so cool'
     )
 
+    Episode.create(
+      id: 2,
+      name: 'Episode 1: Protoss Strikes Back',
+      duration: '1929',
+      feedUrl: 'feed.mp3',
+      podcast: first_podcast,
+      imageUrl: 'terrans.jpg',
+      description: 'The Protoss are so cool'
+    )
+
+    Episode.create(
+      id: 3,
+      name: 'Episode 1: Protoss Strikes Back',
+      duration: '1929',
+      feedUrl: 'feed.mp3',
+      podcast: first_podcast,
+      imageUrl: 'terrans.jpg',
+      description: 'The Protoss are so cool'
+    )
+
     visit '/'
     fill_in 'Email', with: 'harrypotterfan@hogwarts.com'
     fill_in 'Password', with: 'password'
     click_on 'Log in'
     expect(page).to have_content("WELCOME BACK CRAIG")
     click_on 'Generate Suggestions'
-    wait_for_ajax
-    # expect(page).to have_content("YOU HAVEN'T SUBSCRIBED TO ANY PODCASTS. TO DO SO, PLEASE CLICK ON THE + SIGN IN THE TOP RIGHT CORNER TO SEARCH BY NAME OR CLICK ON GENRES TO BROWSE BY GENRE.")
+    expect(page).to have_content("YOU HAVEN'T SCHEDULED A DOWNTIME FOR YOUR CURRENT TIME. HERE ARE SOME EPISODES FROM YOUR SUBSCRIPTIONS TO LISTEN TO ANYWAY.")
   end
 end
